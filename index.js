c13ee2e4cab6cffa7df60861b17bb1d94f89435f
rubn99 <ruben.moreno.romero@gmail.com>
2023-02-21 10:26:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const config = require('./config');
 
-// Using Twitter library
-const Bot = new Twitter(config);
 // Using Twit library
 const Bot = new Twit(config);
+
+Bot.post('statuses/update', { status: 'TEST' }, function(err, data, response) {
+  console.log(data)
+})
